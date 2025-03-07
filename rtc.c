b3813bbadbad6e5a031acbcfec4bd075586786af
ysahnk <193875306+ysahnk@users.noreply.github.com>
2025-03-07 23:39:03 +0900

    test commit

---
+++ rtc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main() {
+	//FILE *fp = fopen("/dev/rtc", "r");
+	//if (NULL == fp) {
+	//	puts("can not open file by fopen()");
+	//}
+
+	int fd = open("/dev/rtc", O_RDONLY);
+	if (fd < 0) {
+		puts("can not open file");
+		perror("rtc");
+		exit(1);
+	}
+
+	char buffer[256] = { 0 };
+	size_t ret = 0;
+
+	ret = read(fd, buffer, 4);
+	puts(buffer);
+
+	return 0;
+
+}
