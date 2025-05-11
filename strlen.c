68a1b364ddbbeeb1db229ab8b5447251bac4b47a
ysahnk <193875306+ysahnk@users.noreply.github.com>
2025-05-12 01:07:08 +0900

    command line arg support

--- strlen.c
+++ strlen.c
@@ -1,8 +1,14 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-	char *str = "abcde";
+int main(int argc, char *argv[]) {
+	char *str;
+	if (argc == 2) {
+		str = argv[1];
+	} else {
+		str = "placeholder";
+	}
 	printf("the length of %s is %d\n", str, strlen(str));
+
 	return 0;
 }
