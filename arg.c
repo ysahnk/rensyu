6e89499eee98a8f1b6f5ac09bcf7debe2467ae6e
ysahnk <193875306+ysahnk@users.noreply.github.com>
2025-08-18 22:13:27 +0900

    Test interactive staging

--- arg.c
+++ arg.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+// test line 1
+
+// test line 2
 int main(int argc, char *argv[]) {
 	for (int i = 0; i < argc + 10; i++)
 		printf("argv[%d] is %s\n", i, argv[i]);
 
 	return 0;
 }
+// test line 3
